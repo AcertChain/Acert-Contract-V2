b796ce833038e1414389dd4fe5fa4096d2facc05
Jiao Feng <jiaofeng601@gmail.com>
2022-04-12 12:34:59 +0800

    Fix account owner and ID bug
    1. only owner can change address of the Account
    2. only owner can change level of the Account
    3.ID is the primary index of the Account
    4.symbol is the primary index of the Asset

--- contracts/World.sol
+++ contracts/World.sol
@@ -34,21 +34,20 @@ contract World is Context, Ownable, Item {
     event ChangeAssetImage(address _worldOwner, string _symbol, string _image);
     // event 创建Account
     event CreateAccount(uint256 _id, address _address);
-    // event 修改Account _address
+    // event _worldOwner修改Account _address
     event ChangeAccount(
+        address _worldOwner,
         uint256 _id, 
         address _executor,
-        bool _isWorldOwner,
         address _newAddress
     );
     // event 修改Account _isTrustAdmin
     event ChangeAccountTrustAdmin(
         uint256 _id, 
         address _executor,
-        bool _isWorldOwner,
         bool _isTrustAdmin
     );
-    // event 修改Account _level
+    // event _worldOwner修改Account _level
     event ChangeAccountLevel(address _worldOwner, uint256 _id, uint256 _level);
 
     // struct Account
@@ -61,11 +60,11 @@ contract World is Context, Ownable, Item {
 
     // struct Asset
     struct Asset {
+        string _symbol;
         uint8 _standard;
         address _contract;
         string _name;
         string _image;
-        string _symbol;
     }
 
     // avatar最大数量
@@ -73,37 +72,34 @@ contract World is Context, Ownable, Item {
 
     // func 注册cash
     function registerCash(
+        string calldata _symbol
         address _contract,
         string calldata _name,
         string calldata _image,
-        string calldata _symbol
     ) public onlyOwner {}
 
     // func 注册item
     function registerItem(
+        string calldata _symbol
         address _contract,
         string calldata _name,
         string calldata _image,
-        string calldata _symbol
     ) public onlyOwner {}
 
     // func 修改Asset _contract
     function changeAsset(
-        address _worldOwner,
-        address _contract,
         string calldata _symbol
+        address _contract,
     ) public onlyOwner {}
 
     // func 修改Asset _name
     function changeAssetName(
-        address _worldOwner,
         string calldata _symbol,
         string calldata _name
     ) public onlyOwner {}
 
     // func 修改Asset _image
     function changeAssetImage(
-        address _worldOwner,
         string calldata _symbol,
         string calldata _image
     ) public onlyOwner {}
@@ -113,6 +109,7 @@ contract World is Context, Ownable, Item {
 
     // func 修改Account _address
     function changeAccount(
+        uint256 _id,
         address _newAddress
     ) public onlyOwner {}
 
@@ -120,10 +117,13 @@ contract World is Context, Ownable, Item {
     function changeAccountTrustAdmin(uint256 _id, bool _trust) public {}
 
     // func 修改Account _level
-    function changeAccountLevel(uint256 _level) public onlyOwner {}
+    function changeAccountLevel(uint256 _id, uint256 _level) public onlyOwner {}
 
     // func 获取Account
-    function getAccount(address _address) public view returns (Account memory) {}
+    function getAccount(uint256 _id) public view returns (Account memory) {}
+
+    // func 通过_address 获取Account
+    function getAccountByAddress(address _address) public view returns (Account memory) {}
 
     // func 获取Asset
     function getAsset(string calldata _symbol) public view returns (Asset memory) {}
