7ae919cdb5221dfada6458ff01b5b4ccd3ef9aa7
yanchenxu <yanchenxu@huobi.com>
2022-06-08 11:56:56 +0800

    update Metaverse

--- contracts/Metaverse.sol
+++ contracts/Metaverse.sol
@@ -10,18 +10,54 @@ contract Metaverse is Ownable {
 
     EnumerableSet.AddressSet private worlds;
 
-    event AddWorld(address indexed world);
+    event AddWorld(
+        address indexed world,
+        string name,
+        string icon,
+        string url,
+        string description
+    );
+    event UpdateWorld(
+        address indexed world,
+        string name,
+        string icon,
+        string url,
+        string description
+    );
     event RemoveWorld(address indexed world);
+
+    mapping(address => WorldInfo) private worldInfos;
+
+    struct WorldInfo {
+        address world;
+        string name;
+        string icon;
+        string url;
+        string description;
+    }
 
     constructor() {
         _owner = msg.sender;
     }
 
-    function addWorld(address _world) public onlyOwner {
+    function addWorld(
+        address _world,
+        string calldata _name,
+        string calldata _icon,
+        string calldata _url,
+        string calldata _description
+    ) public onlyOwner {
         require(_world != address(0), "Metaverse: zero address");
         if (!worlds.contains(_world)) {
             worlds.add(_world);
-            emit AddWorld(_world);
+            worldInfos[_world] = WorldInfo({
+                world: _world,
+                name: _name,
+                icon: _icon,
+                url: _url,
+                description: _description
+            });
+            emit AddWorld(_world,_name,_icon,_url,_description);
         }
     }
 
@@ -30,6 +66,36 @@ contract Metaverse is Ownable {
         if (worlds.contains(_world)) {
             worlds.remove(_world);
             emit RemoveWorld(_world);
+        }
+    }
+
+    function updateWorldInfo(
+        address _world,
+        string calldata _name,
+        string calldata _icon,
+        string calldata _url,
+        string calldata _description
+    ) public onlyOwner {
+        require(_world != address(0), "Metaverse: zero address");
+        if (worlds.contains(_world)) {
+            worldInfos[_world] = WorldInfo({
+                world: _world,
+                name: _name,
+                icon: _icon,
+                url: _url,
+                description: _description
+            });
+            emit UpdateWorld(_world,_name,_icon,_url,_description);
+        }
+    }
+
+    function getWorldInfo(address _world)
+        public
+        view
+        returns (WorldInfo memory info)
+    {
+        if (worlds.contains(_world)) {
+            info = worldInfos[_world];
         }
     }
 
--- test/Metaverse.js
+++ test/Metaverse.js
@@ -24,16 +24,20 @@ contract('Metaverse', function (accounts) {
         describe('addWorld ', function () {
             it('zero address should return revert', async function () {
                 await expectRevert(
-                    this.Metaverse.addWorld(ZERO_ADDRESS), 'Metaverse: zero address',
+                    this.Metaverse.addWorld(ZERO_ADDRESS,"","","",""), 'Metaverse: zero address',
                 );
             });
 
             it('return event ', async function () {
                 const [world] = accounts;
                 expectRevert(
-                    await this.Metaverse.addWorld(world),
+                    await this.Metaverse.addWorld(world,"1","2","3","4"),
                     'AddWorld', {
-                        world
+                        world,
+                        name: "1",
+                        icon: "2",
+                        url:"3",
+                        description:"4"
                     },
                 );
             });
@@ -49,7 +53,7 @@ contract('Metaverse', function (accounts) {
 
             it('return event ', async function () {
                 const [world] = accounts;
-                await this.Metaverse.addWorld(world);
+                await this.Metaverse.addWorld(world,"","","","");
                 expectRevert(
                     await this.Metaverse.removeWorld(world),
                     'removeWorld', {
@@ -60,16 +64,40 @@ contract('Metaverse', function (accounts) {
 
         });
 
-        describe('containsWorld, getWorlds, getWorldCount', function () {
+        describe('updateWorldInfo ', function () {
+            it('zero address should return revert', async function () {
+                await expectRevert(
+                    this.Metaverse.updateWorldInfo(ZERO_ADDRESS,"","","",""), 'Metaverse: zero address',
+                );
+            });
+
+            it('return event ', async function () {
+                const [world] = accounts;
+                await this.Metaverse.addWorld(world,"","","","");
+                expectRevert(
+                    await this.Metaverse.updateWorldInfo(world,"1","2","3","4"),
+                    'UpdateWorld', {
+                        world,
+                        name: "1",
+                        icon: "2",
+                        url:"3",
+                        description:"4"
+                    },
+                );
+            });
+
+        });
+
+        describe('containsWorld, getWorlds, getWorldCount, getWorldInfo', function () {
             beforeEach(async function () {
                 this.world = accounts[0];
-                await this.Metaverse.addWorld(this.world);
+                await this.Metaverse.addWorld(this.world,"","","","");
             });
 
             it('containsWorld', async function () {
                 expect(await this.Metaverse.containsWorld(this.world)).to.be.equal(true);
                 newWorld = accounts[1];
-                await this.Metaverse.addWorld(newWorld);
+                await this.Metaverse.addWorld(newWorld,"","","","");
                 expect(await this.Metaverse.containsWorld(newWorld)).to.be.equal(true);
                 await this.Metaverse.removeWorld(newWorld);
                 expect(await this.Metaverse.containsWorld(newWorld)).to.be.equal(false);
@@ -79,17 +107,23 @@ contract('Metaverse', function (accounts) {
             it('getWorlds', async function () {
                 expect(await this.Metaverse.getWorlds()).to.have.ordered.members([this.world]);
                 newWorld = accounts[1];
-                await this.Metaverse.addWorld(newWorld);
+                await this.Metaverse.addWorld(newWorld,"","","","");
                 expect(await this.Metaverse.getWorlds()).to.have.ordered.members([this.world, newWorld]);
 
             });
             it('getWorldCount', async function () {
                 expect(await this.Metaverse.getWorldCount()).to.be.bignumber.equal(new BN(1));
                 newWorld = accounts[1];
-                await this.Metaverse.addWorld(newWorld);
+                await this.Metaverse.addWorld(newWorld,"","","","");
                 expect(await this.Metaverse.getWorldCount()).to.be.bignumber.equal(new BN(2));
                 await this.Metaverse.removeWorld(newWorld);
                 expect(await this.Metaverse.getWorldCount()).to.be.bignumber.equal(new BN(1));
+            });
+
+            it('getWorldInfo', async function () {
+                newWorld = accounts[1];
+                await this.Metaverse.addWorld(newWorld,"1","2","3","4");
+                expect(await this.Metaverse.getWorldInfo(newWorld)).have.ordered.members([newWorld, "1", "2", "3", "4"]);
             });
 
         });
