6898d2666ed7e579e90db2368b396bd764395bf5
Jiao Feng <jiaofeng601@gmail.com>
2022-06-10 04:14:07 +0800

    Update IItem721BWO.sol
            address indexed sender,
            uint256 indexed nonce,
            uint256 indexed deadline

--- contracts/interfaces/IItem721BWO.sol
+++ contracts/interfaces/IItem721BWO.sol
@@ -6,69 +6,71 @@ interface IItem721BWO {
         uint256 indexed from,
         uint256 indexed to,
         uint256 indexed tokenId,
-        uint256 nonce
+        address indexed sender,
+        uint256 indexed nonce,
+        uint256 indexed deadline
     );
 
     event ApprovalItemBWO(
-        uint256 indexed owner,
-        address indexed approved,
+        address indexed to,
         uint256 indexed tokenId,
-        uint256 nonce
+        address indexed sender,
+        uint256 indexed nonce,
+        uint256 indexed deadline
     );
 
     event ApprovalForAllItemBWO(
-        uint256 indexed owner,
-        address indexed operator,
+        uint256 indexed from,
+        address indexed to,
         bool approved,
-        uint256 nonce
+        address indexed sender,
+        uint256 indexed nonce,
+        uint256 indexed deadline
     );
 
     function safeTransferFromItemBWO(
-        uint256 spender,
-        uint256 from,
-        uint256 to,
-        uint256 tokenId,
-        address sender,
-        uint256 deadline,
+        uint256 indexed from,
+        uint256 indexed to,
+        uint256 indexed tokenId,
         bytes memory data,
-        bytes memory signature
+        address indexed sender,
+        uint256 indexed nonce,
+        uint256 indexed deadline
     ) external;
 
     function safeTransferFromItemBWO(
-        uint256 spender,
-        uint256 from,
-        uint256 to,
-        uint256 tokenId,
-        address sender,
-        uint256 deadline,
-        bytes memory signature
+        uint256 indexed from,
+        uint256 indexed to,
+        uint256 indexed tokenId,
+        address indexed sender,
+        uint256 indexed nonce,
+        uint256 indexed deadline
     ) external;
 
     function transferFromItemBWO(
-        uint256 spender,
-        uint256 from,
-        uint256 to,
-        uint256 tokenId,
-        address sender,
-        uint256 deadline,
-        bytes memory signature
+        uint256 indexed from,
+        uint256 indexed to,
+        uint256 indexed tokenId,
+        address indexed sender,
+        uint256 indexed nonce,
+        uint256 indexed deadline
     ) external;
 
     function approveItemBWO(
-        uint256 from,
-        address to,
-        uint256 tokenId,
-        address sender,
-        uint256 deadline,
-        bytes memory signature
+        uint256 indexed from,
+        address indexed to,
+        uint256 indexed tokenId,
+        address indexed sender,
+        uint256 indexed nonce,
+        uint256 indexed deadline
     ) external;
 
     function setApprovalForAllItemBWO(
-        uint256 from,
-        address to,
+        uint256 indexed from,
+        address indexed to,
         bool approved,
-        address sender,
-        uint256 deadline,
-        bytes memory signature
+        address indexed sender,
+        uint256 indexed nonce,
+        uint256 indexed deadline
     ) external;
 }
