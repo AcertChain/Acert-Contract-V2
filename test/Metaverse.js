1c5b0a04a24f0d0e68c528d274c16582f67d604b
yanchenxu <yanchenxu@huobi.com>
2022-06-19 00:04:16 +0800

    update metaverse test

--- test/Metaverse.js
+++ test/Metaverse.js
@@ -14,10 +14,12 @@ const {
 } = constants;
 
 const Metaverse = artifacts.require('Metaverse');
+const World = artifacts.require('World');
 
 contract('Metaverse', function (accounts) {
     beforeEach(async function () {
-        this.Metaverse = await Metaverse.new("metaverse","1.0");
+        this.Metaverse = await Metaverse.new("metaverse", "1.0");
+        this.world = await World.new(this.Metaverse.address);
     });
 
     context('测试Metaverse 功能', function () {
@@ -29,11 +31,10 @@ contract('Metaverse', function (accounts) {
             });
 
             it('return event ', async function () {
-                const [world] = accounts;
                 expectRevert(
-                    await this.Metaverse.addWorld(world, "1", "2", "3", "4"),
+                    await this.Metaverse.addWorld(this.world.address, "1", "2", "3", "4"),
                     'AddWorld', {
-                        world,
+                        world: this.world.address,
                         name: "1",
                         icon: "2",
                         url: "3",
@@ -52,12 +53,11 @@ contract('Metaverse', function (accounts) {
             });
 
             it('return event ', async function () {
-                const [world] = accounts;
-                await this.Metaverse.addWorld(world, "", "", "", "");
+                await this.Metaverse.addWorld(this.world.address, "", "", "", "");
                 expectRevert(
-                    await this.Metaverse.removeWorld(world),
+                    await this.Metaverse.removeWorld(this.world.address),
                     'removeWorld', {
-                        world
+                        world: this.world.address
                     },
                 );
             });
@@ -72,12 +72,11 @@ contract('Metaverse', function (accounts) {
             });
 
             it('return event ', async function () {
-                const [world] = accounts;
-                await this.Metaverse.addWorld(world, "", "", "", "");
+                await this.Metaverse.addWorld(this.world.address, "", "", "", "");
                 expectRevert(
-                    await this.Metaverse.updateWorldInfo(world, "1", "2", "3", "4"),
+                    await this.Metaverse.updateWorldInfo(this.world.address, "1", "2", "3", "4"),
                     'UpdateWorld', {
-                        world,
+                        world: this.world.address,
                         name: "1",
                         icon: "2",
                         url: "3",
@@ -88,92 +87,123 @@ contract('Metaverse', function (accounts) {
 
         });
 
+        describe('setAdmin', function () {
+            it('zero address should return revert', async function () {
+                await expectRevert(
+                    this.Metaverse.setAdmin(ZERO_ADDRESS), 'Metaverse: zero address',
+                );
+            });
+
+            it('return event ', async function () {
+                const [admin] = accounts;
+                expectRevert(
+                    await this.Metaverse.setAdmin(admin),
+                    'SetAdmin', {
+                        admin
+                    },
+                );
+            });
+        });
+
+        describe('addOperator', function () {
+            it('zero address should return revert', async function () {
+                await expectRevert(
+                    this.Metaverse.addOperator(ZERO_ADDRESS), 'Metaverse: zero address',
+                );
+            });
+
+            it('return event ', async function () {
+                const [operator] = accounts;
+                expectRevert(
+                    await this.Metaverse.addOperator(operator),
+                    'AddOperator', {
+                        operator
+                    },
+                );
+            });
+        });
+
+        describe('removeOperator', function () {
+            it('return event ', async function () {
+                const [operator] = accounts;
+                await this.Metaverse.addOperator(operator);
+                expectRevert(
+                    await this.Metaverse.removeOperator(operator),
+                    'RemoveOperator', {
+                        operator
+                    },
+                );
+            });
+        });
+
+        describe('isOperator,isBWO', function () {
+            it('check', async function () {
+                const [operator] = accounts;
+                expect(await this.Metaverse.isOperator(operator)).to.be.equal(false);
+                expect(await this.Metaverse.isBWO(operator)).to.be.equal(false);
+                await this.Metaverse.addOperator(operator)
+                expect(await this.Metaverse.isOperator(operator)).to.be.equal(true);
+                expect(await this.Metaverse.isBWO(operator)).to.be.equal(true);
+            });
+
+        });
+
         describe('containsWorld, getWorlds, getWorldCount, getWorldInfo', function () {
             beforeEach(async function () {
-                this.world = accounts[0];
-                await this.Metaverse.addWorld(this.world, "", "", "", "");
+                await this.Metaverse.addWorld(this.world.address, "1", "2", "3", "4");
             });
 
             it('containsWorld', async function () {
-                expect(await this.Metaverse.containsWorld(this.world)).to.be.equal(true);
-                newWorld = accounts[1];
-                await this.Metaverse.addWorld(newWorld, "", "", "", "");
-                expect(await this.Metaverse.containsWorld(newWorld)).to.be.equal(true);
-                await this.Metaverse.removeWorld(newWorld);
-                expect(await this.Metaverse.containsWorld(newWorld)).to.be.equal(false);
-
+                expect(await this.Metaverse.containsWorld(this.world.address)).to.be.equal(true);
+                await this.Metaverse.removeWorld(this.world.address);
+                expect(await this.Metaverse.containsWorld(this.world.address)).to.be.equal(false);
             });
 
             it('getWorlds', async function () {
-                expect(await this.Metaverse.getWorlds()).to.have.ordered.members([this.world]);
-                newWorld = accounts[1];
-                await this.Metaverse.addWorld(newWorld, "", "", "", "");
-                expect(await this.Metaverse.getWorlds()).to.have.ordered.members([this.world, newWorld]);
+                expect(await this.Metaverse.getWorlds()).to.have.ordered.members([this.world.address]);
 
             });
             it('getWorldCount', async function () {
                 expect(await this.Metaverse.getWorldCount()).to.be.bignumber.equal(new BN(1));
-                newWorld = accounts[1];
-                await this.Metaverse.addWorld(newWorld, "", "", "", "");
-                expect(await this.Metaverse.getWorldCount()).to.be.bignumber.equal(new BN(2));
-                await this.Metaverse.removeWorld(newWorld);
-                expect(await this.Metaverse.getWorldCount()).to.be.bignumber.equal(new BN(1));
             });
 
             it('getWorldInfo', async function () {
-                newWorld = accounts[1];
-                await this.Metaverse.addWorld(newWorld, "1", "2", "3", "4");
-                expect(await this.Metaverse.getWorldInfo(newWorld)).have.ordered.members([newWorld, "1", "2", "3", "4"]);
+
+                expect(await this.Metaverse.getWorldInfo(this.world.address)).have.ordered.members([this.world.address, "1", "2", "3", "4"]);
             });
         });
     });
- });
 
-
- function shouldBehaveLikeWorldAccount(account, newAccount, operator) {
-    context('World Account', function () {
+    context('Account', function () {
         describe('getOrCreateAccountId', function () {
             context('create account ', function () {
                 it('carete account event ', async function () {
+                    const [account] = accounts;
                     expectEvent(await this.Metaverse.getOrCreateAccountId(account), 'CreateAccount', {
-                        id: new BN(await this.Metaverse.getAccountIdByAddress(account)),
+                        id: new BN(await this.Metaverse.getIdByAddress(account)),
                         account: account
                     });
                 });
             });
         });
-        describe('getAccountIdByAddress', function () {
+        describe('getIdByAddress', function () {
             context('get account id by address', function () {
                 it('equal 101', async function () {
+                    const [account] = accounts;
                     await this.Metaverse.getOrCreateAccountId(account)
-                    const avatarMaxId = new BN(await this.avatar.maxAvatar());
-                    expect(await this.Metaverse.getAccountIdByAddress(account)).to.bignumber.equal(avatarMaxId.add(new BN(1)));
+                    expect(await this.Metaverse.getIdByAddress(account)).to.bignumber.equal(new BN(1));
                 });
             });
         });
         describe('getAddressById', function () {
-            context('get avatar id', function () {
-
-                it('avatar id is not exist', async function () {
-                    await expectRevert(this.Metaverse.getAddressById(new BN(1)), 'Item: owner query for nonexistent token');
-                });
-
-                it('avatar id is exist', async function () {
-                    await this.Metaverse.getOrCreateAccountId(account)
-                    await this.avatar.mint(account, new BN(1));
-                    expect(await this.Metaverse.getAddressById(new BN(1))).to.equal(account);
-                });
-            });
-
             context('get account id', function () {
                 it('account id is not exist', async function () {
-                    const avatarMaxId = new BN(await this.avatar.maxAvatar());
-                    expect(await this.Metaverse.getAddressById(avatarMaxId.add(new BN(1)))).to.equal(ZERO_ADDRESS);
+                    expect(await this.Metaverse.getAddressById(new BN(1))).to.equal(ZERO_ADDRESS);
                 });
                 it('account id is exist', async function () {
+                    const [account] = accounts;
                     await this.Metaverse.getOrCreateAccountId(account)
-                    const avatarMaxId = new BN(await this.avatar.maxAvatar());
-                    expect(await this.Metaverse.getAddressById(avatarMaxId.add(new BN(1)))).to.equal(account);
+                    expect(await this.Metaverse.getAddressById(new BN(1))).to.equal(account);
                 });
             });
         });
@@ -181,14 +211,16 @@ contract('Metaverse', function (accounts) {
         describe('createAccount', function () {
             context('create account ', function () {
                 it('zero address', async function () {
+                    const [account] = accounts;
                     await expectRevert(this.Metaverse.createAccount(ZERO_ADDRESS, {
                         from: account
-                    }), 'World: zero address');
+                    }), 'Metaverse: zero address');
 
                 });
                 it('is address exist', async function () {
-                    await this.Metaverse.createAccount(account)
-                    await expectRevert(this.Metaverse.createAccount(account), "World: address is exist");
+                    const [account] = accounts;
+                    await this.Metaverse.createAccount(account, true)
+                    await expectRevert(this.Metaverse.createAccount(account, true), "Metaverse: address is exist");
                 });
             });
         });
@@ -196,24 +228,72 @@ contract('Metaverse', function (accounts) {
         describe('changeAccount', function () {
             context('ueser change account', function () {
                 it('is not contract owner', async function () {
+                    const [account, newAccount] = accounts;
                     await this.Metaverse.getOrCreateAccountId(account)
-                    const accountId = new BN(await this.Metaverse.getAccountIdByAddress(account));
+                    const accountId = new BN(await this.Metaverse.getIdByAddress(account));
 
-                    await expectRevert(this.Metaverse.changeAccount(accountId, newAccount, true, {
+                    await expectRevert(this.Metaverse.changeAccount(accountId, newAccount, true,  {
                         from: newAccount
-                    }), 'only owner');
+                    }), 'Metaverse: sender not owner or admin');
                 });
 
                 it('is owner', async function () {
+                    const [account, newAccount] = accounts;
+
                     await this.Metaverse.getOrCreateAccountId(account)
-                    const accountId = new BN(await this.Metaverse.getAccountIdByAddress(account));
-                    expectEvent(await this.Metaverse.changeAccount(accountId, newAccount, true), 'UpdateAccount', {
+                    const accountId = new BN(await this.Metaverse.getIdByAddress(account));
+                    expectEvent(await this.Metaverse.changeAccount(accountId, newAccount, true, {
+                        from: account
+                    }), 'UpdateAccount', {
                         id: accountId,
                         newAddress: newAccount,
-                        isTrust: true
+                        isTrustAdmin: true
                     });
                 });
             });
         });
+
+        describe('freezeAccount', function () {
+            it('return event', async function () {
+
+                const [account] = accounts;
+                await this.Metaverse.getOrCreateAccountId(account);
+                const accountId = new BN(await this.Metaverse.getIdByAddress(account));
+
+                expectEvent(await this.Metaverse.freezeAccount(accountId, {
+                    from: account
+                }), 'FreezeAccount', {
+                    id: accountId
+                });
+
+                expect(await this.Metaverse.isFreeze(accountId)).to.be.equal(true);
+            });
+        });
+
+        describe('unfreezeAccount', function () {
+            it('return event', async function () {
+
+                const [account, admin] = accounts;
+                await this.Metaverse.setAdmin(admin);
+
+                await this.Metaverse.getOrCreateAccountId(account);
+                const accountId = new BN(await this.Metaverse.getIdByAddress(account));
+                expect(await this.Metaverse.isFreeze(accountId)).to.be.equal(false);
+
+                await this.Metaverse.freezeAccount(accountId, {
+                    from: account
+                });
+                expect(await this.Metaverse.isFreeze(accountId)).to.be.equal(true);
+
+                expectEvent(await this.Metaverse.unfreezeAccount(accountId, {
+                    from: admin
+                }), 'UnFreezeAccount', {
+                    id: accountId
+                });
+                expect(await this.Metaverse.isFreeze(accountId)).to.be.equal(false);
+
+            });
+
+        });
     });
-}
+});
