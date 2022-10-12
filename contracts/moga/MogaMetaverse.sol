568f0e5179a2cff7db8d87225ef08e096ff1d3a1
jiaofeng <jiaofeng601@gmail.com>
2022-10-12 14:51:07 +0800

    move some events to IMetaverse & IWorld

--- contracts/moga/MogaMetaverse.sol
+++ contracts/moga/MogaMetaverse.sol
@@ -11,27 +11,9 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
 contract MogaMetaverse is IMetaverse, IApplyStorage, Context, Ownable, EIP712 {
-    enum OperationEnum {
-        ADD,
-        REMOVE
-    }
-    event RegisterWorld(address indexed world, string name, address indexed storageAddress);
-    event DisableWorld(address indexed world);
     event SetAdmin(address indexed admin);
     event AddOperator(address indexed operator);
     event RemoveOperator(address indexed operator);
-    event CreateAccount(uint256 indexed accountId, address indexed authAddress, bool isTrustAdmin);
-    event TrustAdmin(uint256 indexed accountId, bool isTrustAdmin, bool isBWO, address indexed Sender, uint256 nonce);
-    event FreezeAccount(uint256 indexed accountId, bool isBWO, address indexed Sender, uint256 nonce);
-    event UnFreezeAccount(uint256 indexed accountId, address indexed newAuthAddress);
-    event AuthAddressChanged(
-        uint256 indexed accountId,
-        address indexed authAddress,
-        OperationEnum operation,
-        bool isBWO,
-        address indexed sender,
-        uint256 nonce
-    );
 
     string public override name;
     address public admin;
