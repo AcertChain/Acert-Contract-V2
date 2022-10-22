f338271aa0bc6b25e5c895d65244ec70d7df5fdc
jiaofeng <jiaofeng601@gmail.com>
2022-10-22 20:48:43 +0800

    add Acert.sol

---
+++ contracts/Acert.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "./interfaces/IMetaverse.sol";
+import "./interfaces/IApplyStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Acert is Ownable {
+    event SetMetaverse(address indexed metaverse, string name, address indexed storageAddress, bool enabled);
+
+    mapping(address => bool) public metaverseEnabled;
+
+    constructor() {
+    }
+
+    function setMetaverse(address _address, bool _enabled) public onlyOwner {
+        metaverseEnabled[_address] = _enabled;
+        string memory name = IMetaverse(_address).name();
+        address storageAddress = IApplyStorage(_address).getStorageAddress();
+        emit SetMetaverse(_address, name, storageAddress, _enabled);
+    }
+}
