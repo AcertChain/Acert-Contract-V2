9f783637bc5dfdddc19fb855a6030afc6ba09d76
jiaofeng <jiaofeng601@gmail.com>
2022-10-23 21:18:09 +0800

    refactoring INFTMetadata

--- contracts/interfaces/INFTMetadata.sol
+++ contracts/interfaces/INFTMetadata.sol
@@ -3,48 +3,32 @@ pragma solidity ^0.8.0;
 
 interface INFTMetadata {
     event SetString(address indexed assetContract, uint256 indexed tokenId, string key, string value);
-    event DeleteStringByKey(address indexed assetContract, uint256 indexed tokenId, string key);
 
-    event SetUint256(address indexed assetContract, uint256 indexed tokenId, string key, uint256 value);
-    event DeleteUint256ByKey(address indexed assetContract, uint256 indexed tokenId, string key);
+    event SetMetadata(address indexed assetContract, uint256 indexed tokenId, string key, uint256 value, string valueType);
+
+    event RemoveMetadata(address indexed assetContract, uint256 indexed tokenId, string key);
 
     event ClearMetadata(address indexed assetContract, uint256 indexed tokenId);
 
-    function setString(
+    function setMetadata(
         uint256 tokenId,
         string memory key,
-        string memory value
+        string memory value,
+        string memory valueType
     ) external returns (bool);
 
-    function setStringBatch(
+    function batchSetMetadata(
         uint256[] memory tokenId,
-        string[][] memory key,
-        string[][] memory value
+        string[][] memory keys,
+        string[][] memory values,
+        string[][] memory valueTypes
     ) external returns (bool);
 
-    function deleteString(uint256 tokenId, string memory key) external returns (bool);
+    function getKeys(uint256 tokenId) external returns (string[] memory);
 
-    function getStringKeys(uint256 tokenId) external returns (string[] memory);
+    function getValue(uint256 tokenId, string memory key) external returns (string memory);
 
-    function getString(uint256 tokenId, string memory key) external returns (string memory);
-
-    function setUint256(
-        uint256 tokenId,
-        string memory key,
-        uint256 value
-    ) external returns (bool);
-
-    function setUint256Batch(
-        uint256[] memory tokenId,
-        string[][] memory key,
-        uint256[][] memory value
-    ) external returns (bool);
-
-    function deleteUint256(uint256 tokenId, string memory key) external returns (bool);
-
-    function getUint256Keys(uint256 tokenId) external returns (string[] memory);
-
-    function getUint256(uint256 tokenId, string memory key) external returns (uint256);
+    function removeMetadata(uint256 tokenId, string memory key) external returns (bool);
 
     function clearMetadata(uint256 tokenId) external returns (bool);
 
